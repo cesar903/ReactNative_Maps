a99f8b7fbbfa17f3e883325d3f45e8cc1a34cf2c
cesar903 <cesarreis521@gmai.com>
2024-08-09 16:40:30 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react"
+import { View, Text, StatusBar, StyleSheet } from "react-native"
+import * as Location from "expo-location"
+import MapView from 'react-native-maps';
+
+const estilo = StyleSheet.create({
+  tela: { flex: 1 },
+  indicador: { backgroundColor: "#144272", padding: 32 },
+  indicardorTexto: { color: "white", fontSize: 20 },
+  mapa: { height: "100%", width: "100%" }
+})
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [localizacao, definirLocalizacao] = useState({})
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(function () {
+    async function ObterLocalizacao() {
+      await Location.requestForegroundPermissionsAsync()
+      definirLocalizacao(await Location.getCurrentPositionAsync(
+
+      ))
+    }
+    ObterLocalizacao()
+  }, [])
+
+  return <View style={ estilo.tela }>
+    <StatusBar barStyle="light-content" backgroundColor="#144272" />
+    {Object.keys(localizacao).length > 0 &&
+      <>
+        <View style={ estilo.indicador }>
+          <Text style={ estilo.indicardorTexto }> EXPLORAR NOVOS MAPAS </Text>
+          <Text style={ estilo.indicardorTexto }> Latitude: {localizacao.coords.latitude} </Text>
+          <Text style={ estilo.indicardorTexto }> Longitude: {localizacao.coords.longitude} </Text>
+          <Text style={ estilo.indicardorTexto }> Altitude: {localizacao.coords.altitude} </Text>
+        </View>
+        <MapView
+          initialRegion={{
+            latitude: localizacao.coords.latitude,
+            longitude: localizacao.coords.longitude,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01,
+          }}
+          style={estilo.mapa} />
+      </>
+    }
+  </View>
+}
